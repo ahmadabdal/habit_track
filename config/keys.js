8f06dfe7697e0c600306aaa0fce021918603af19
ahmadabdal <ahmadabdal675@gmail.com>
2023-03-18 11:01:40 +0530

    complated

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase =()=>{
-    mongoose.connect(process.env.uri,{ useNewUrlParser: true }).then(()=>{
+    mongoose.connect(process.env.URL,{ useNewUrlParser: true ,useUnifiedTopology: true }).then(()=>{
         console.log(`Mongodb connected with server: `);
     }).catch((err)=>{
         console.log(err);
